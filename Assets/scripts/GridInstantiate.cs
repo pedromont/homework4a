187b1a872d9ae560f3f14971711a8f95028a1f25
Pedro Mont <pm2558@nyu.com>
2015-07-29 19:45:08 -0400

    1st pass

---
+++ Assets/scripts/GridInstantiate.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GridInstantiate : MonoBehaviour {
+	
+	public Transform floorPrefab2;
+	public Transform wallPrefab2;
+	private float randomp;
+
+	// Use this for initialization
+	void Start () {
+
+		for (int x = 0; x<5; x++) {
+			for (int z = 0; z<5 ; z++){
+				//pos += new Vector3 (x * 5f, 0f ,z * 5f);
+				randomp = Random.Range(0f, 1f);
+				Vector3 poss = new Vector3 (x * 5f, 0f ,z * 5f);
+				//transform.position Vector3 pos;
+
+				if(randomp>0.30f){
+					Transform newClone = (Transform)Instantiate (floorPrefab2, poss, transform.rotation);
+				}
+				if(randomp>0.05f && randomp<0.3f){
+					Transform newClone = (Transform)Instantiate (wallPrefab2, poss, transform.rotation);
+				}
+
+			} 
+		}
+
+
+		//Destroy (this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/scripts/PathInstantiate.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathInstantiate : MonoBehaviour {
+
+	private int counter;
+	public Transform floorPrefab;
+
+	// Use this for initialization
+	void Start () {
+		counter = 0;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (counter < 50) {
+			counter++;
+			float randomr = Random.Range (0f, 1f);
+			if (randomr < 0.25f) {
+				transform.Rotate (0, 90f, 0);
+			} 
+			if (randomr < 0.5f && randomr >=0.25f) {
+				transform.Rotate (0, -90f, 0);
+			} 
+			Transform newClone = (Transform)Instantiate (floorPrefab, transform.position, transform.rotation); 
+			transform.Translate (5f, 0f, 0f);
+			counter++;
+		} else {
+			Destroy (this.gameObject);
+		}
+	}
+}
